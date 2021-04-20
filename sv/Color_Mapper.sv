c3aab58d810091e5d06a02d908954c7278704be1
ngandi2 <46947884+ngandi2@users.noreply.github.com>
2021-04-19 22:23:47 -0500

    Add files via upload
    initial commit - stage ram

---
+++ sv/Color_Mapper.sv
@@ -0,0 +1,59 @@
+//-------------------------------------------------------------------------
+//    Color_Mapper.sv                                                    --
+//    Stephen Kempf                                                      --
+//    3-1-06                                                             --
+//                                                                       --
+//    Modified by David Kesler  07-16-2008                               --
+//    Translated by Joe Meng    07-07-2013                               --
+//                                                                       --
+//    Fall 2014 Distribution                                             --
+//                                                                       --
+//    For use with ECE 385 Lab 7                                         --
+//    University of Illinois ECE Department                              --
+//-------------------------------------------------------------------------
+
+
+module  color_mapper (
+    input [9:0] DrawX, DrawY, 
+    input logic [1:0] stage_color_index,
+    output logic [7:0] Red, Green, Blue 
+);
+    // 208 x 200
+    // ['0x000000', '0xFFFFFF', '0x0000FF', '0xB6B6AA']
+    always_comb
+    begin
+        case (stage_color_index)
+            2'b00:
+            begin
+                Red = 8'h00;
+                Green = 8'h00;
+                Blue = 8'h00;
+            end
+            2'b01:
+            begin
+                Red = 8'hFF;
+                Green = 8'hFF;
+                Blue = 8'hFF;
+            end
+            2'b10:
+            begin
+                Red = 8'h00;
+                Green = 8'h00;
+                Blue = 8'hFF;
+            end
+            2'b11:
+            begin
+                Red = 8'hB6;
+                Green = 8'hB6;
+                Blue = 8'hAA;
+            end
+            default:
+            begin
+                Red = 8'h00;
+                Green = 8'h00;
+                Blue = 8'h00;
+            end
+			endcase
+    end
+    
+endmodule
---
+++ sv/stage_ram.sv
@@ -0,0 +1,34 @@
+/*
+ * ECE385-HelperTools/PNG-To-Txt
+ * Author: Rishi Thakkar
+ *
+ */
+
+module stageRAM
+(
+	input [1:0] data_In,
+	input [18:0] write_address, read_address,
+	input we, Clk,
+	output logic [1:0] data_Out
+);
+
+// mem has width of 2 bits and a total of 261120 addresses
+// ['0x000000', '0xFFFFFF', '0x0000FF', '0xB6B6AA']
+logic [1:0] mem [0:261119];
+
+initial
+begin
+	 $readmemh("sprite_bytes/stages.txt", mem);
+end
+
+
+always_ff @ (posedge Clk) begin
+	if (we)
+		mem[write_address] <= data_In;
+	if(read_address < 261120)
+		data_Out <= mem[read_address];
+	else
+		data_Out <= '0;
+end
+
+endmodule
