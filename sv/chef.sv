c221083b77fe2a683ad108905f3f2826eeeccb12
David <dnh2@illinois.edu>
2021-04-22 01:19:08 -0500

    added chef freeform movement, need to restrict

---
+++ sv/chef.sv
@@ -0,0 +1,90 @@
+module  chef (
+	 input Reset, frame_clk,
+	 input [7:0] keycode,
+    output [9:0] ChefX, ChefY
+);
+    
+    logic [9:0] Chef_X_Pos, Chef_X_Motion, Chef_Y_Pos, Chef_Y_Motion, Chef_Size;
+	 
+    parameter [9:0] Chef_X_Center=96;   // Center position on the X axis
+    parameter [9:0] Chef_Y_Center=141;  // Center position on the Y axis
+    parameter [9:0] Chef_X_Min=0;       // Leftmost point on the X axis
+    parameter [9:0] Chef_X_Max=192;     // Rightmost point on the X axis
+    parameter [9:0] Chef_Y_Min=0;       // Topmost point on the Y axis
+    parameter [9:0] Chef_Y_Max=216;     // Bottommost point on the Y axis
+	 parameter [9:0] Chef_X_Step=1;      // Step size on the X axis
+    parameter [9:0] Chef_Y_Step=1;      // Step size on the Y axis
+
+    assign Chef_Size = 1;
+   
+    always_ff @ (posedge Reset or posedge frame_clk)
+    begin: Move_Chef
+        if (Reset)  // Asynchronous Reset
+        begin 
+            Chef_Y_Motion <= 10'd0; 
+				Chef_X_Motion <= 10'd0;
+				Chef_Y_Pos <= Chef_Y_Center;
+				Chef_X_Pos <= Chef_X_Center;
+        end
+           
+        else 
+        begin 
+				 // Not sure how to make it stop bouncing without other glitches...
+				 if ( (Chef_Y_Pos + Chef_Size) >= Chef_Y_Max )  // Chef is at the bottom edge
+					  Chef_Y_Motion <= (~ (Chef_Y_Step) + 1'b1); 
+					  
+				 else if ( (Chef_Y_Pos - Chef_Size) <= Chef_Y_Min )  // Chef is at the top edge
+					  Chef_Y_Motion <= Chef_Y_Step;
+					  
+				 else if ( (Chef_X_Pos + Chef_Size) >= Chef_X_Max )  // Chef is at the right edge
+					  Chef_X_Motion <= (~ (Chef_X_Step) + 1'b1); 
+					  
+				 else if ( (Chef_X_Pos - Chef_Size) <= Chef_X_Min )  // Chef is at the left edge
+					  Chef_X_Motion <= Chef_X_Step;
+					  
+				 else 
+				 begin
+					  Chef_X_Motion <= 0;
+					  Chef_Y_Motion <= 0;
+					  
+				 
+				 case (keycode)
+					8'h04 : begin
+							  Chef_X_Motion <= -1; // A
+							  Chef_Y_Motion <= 0;
+							  end
+					        
+					8'h07 : begin
+					        Chef_X_Motion <= 1; // D
+							  Chef_Y_Motion <= 0;
+							  end
+							  
+					8'h16 : begin
+					        Chef_Y_Motion <= 1; // S
+							  Chef_X_Motion <= 0;
+							  end
+							  
+					8'h1A : begin
+					        Chef_Y_Motion <= -1; // W
+							  Chef_X_Motion <= 0;
+							  end	  
+							  
+					default : begin
+					          Chef_Y_Motion <= 0; // Anything else
+							    Chef_X_Motion <= 0;
+							    end
+			   endcase
+				end
+				 
+				 Chef_Y_Pos <= (Chef_Y_Pos + Chef_Y_Motion);
+				 Chef_X_Pos <= (Chef_X_Pos + Chef_X_Motion);
+      
+			
+			end  
+    end
+       
+    assign ChefX = Chef_X_Pos;
+    assign ChefY = Chef_Y_Pos;
+    
+
+endmodule
--- sv/final_top.sv
+++ sv/final_top.sv
@@ -120,43 +120,43 @@ logic Reset_h, vssig, blank, sync, VGA_Clk;
 	assign VGA_G = Green[7:4];
 	
 	
-	// lab62soc u0 (
-	// 	.clk_clk                           (MAX10_CLK1_50),  //clk.clk
-	// 	.reset_reset_n                     (1'b1),           //reset.reset_n
-	// 	.altpll_0_locked_conduit_export    (),               //altpll_0_locked_conduit.export
-	// 	.altpll_0_phasedone_conduit_export (),               //altpll_0_phasedone_conduit.export
-	// 	.altpll_0_areset_conduit_export    (),               //altpll_0_areset_conduit.export
-	// 	.key_external_connection_export    (KEY),            //key_external_connection.export
+	final_soc u0 (
+	 	.clk_clk                           (MAX10_CLK1_50),  //clk.clk
+	 	.reset_reset_n                     (1'b1),           //reset.reset_n
+	 	.altpll_0_locked_conduit_export    (),               //altpll_0_locked_conduit.export
+	 	.altpll_0_phasedone_conduit_export (),               //altpll_0_phasedone_conduit.export
+	 	.altpll_0_areset_conduit_export    (),               //altpll_0_areset_conduit.export
+	 	.key_external_connection_export    (KEY),            //key_external_connection.export
 
-	// 	//SDRAM
-	// 	.sdram_clk_clk(DRAM_CLK),                            //clk_sdram.clk
-	// 	.sdram_wire_addr(DRAM_ADDR),                         //sdram_wire.addr
-	// 	.sdram_wire_ba(DRAM_BA),                             //.ba
-	// 	.sdram_wire_cas_n(DRAM_CAS_N),                       //.cas_n
-	// 	.sdram_wire_cke(DRAM_CKE),                           //.cke
-	// 	.sdram_wire_cs_n(DRAM_CS_N),                         //.cs_n
-	// 	.sdram_wire_dq(DRAM_DQ),                             //.dq
-	// 	.sdram_wire_dqm({DRAM_UDQM,DRAM_LDQM}),              //.dqm
-	// 	.sdram_wire_ras_n(DRAM_RAS_N),                       //.ras_n
-	// 	.sdram_wire_we_n(DRAM_WE_N),                         //.we_n
+	 	//SDRAM
+	 	.sdram_clk_clk(DRAM_CLK),                            //clk_sdram.clk
+	 	.sdram_wire_addr(DRAM_ADDR),                         //sdram_wire.addr
+	 	.sdram_wire_ba(DRAM_BA),                             //.ba
+	 	.sdram_wire_cas_n(DRAM_CAS_N),                       //.cas_n
+	 	.sdram_wire_cke(DRAM_CKE),                           //.cke
+	 	.sdram_wire_cs_n(DRAM_CS_N),                         //.cs_n
+	 	.sdram_wire_dq(DRAM_DQ),                             //.dq
+	 	.sdram_wire_dqm({DRAM_UDQM,DRAM_LDQM}),              //.dqm
+	 	.sdram_wire_ras_n(DRAM_RAS_N),                       //.ras_n
+	 	.sdram_wire_we_n(DRAM_WE_N),                         //.we_n
 
-	// 	//USB SPI	
-	// 	.spi0_SS_n(SPI0_CS_N),
-	// 	.spi0_MOSI(SPI0_MOSI),
-	// 	.spi0_MISO(SPI0_MISO),
-	// 	.spi0_SCLK(SPI0_SCLK),
+	 	//USB SPI	
+	 	.spi_0_SS_n(SPI0_CS_N),
+	 	.spi_0_MOSI(SPI0_MOSI),
+	 	.spi_0_MISO(SPI0_MISO),
+	 	.spi_0_SCLK(SPI0_SCLK),
 		
-	// 	//USB GPIO
-	// 	.usb_rst_export(USB_RST),
-	// 	.usb_irq_export(USB_IRQ),
-	// 	.usb_gpx_export(USB_GPX),
+	 	//USB GPIO
+	 	.usb_rst_export(USB_RST),
+	 	.usb_irq_export(USB_IRQ),
+	 	.usb_gpx_export(USB_GPX),
 		
-	// 	//LEDs and HEX
-	// 	.hex_digits_export({hex_num_4, hex_num_3, hex_num_1, hex_num_0}),
-	// 	.leds_export({hundreds, signs, LEDR}),
-	// 	.keycode_export(keycode)
+	 	//LEDs and HEX
+	 	.hex_digits_export({hex_num_4, hex_num_3, hex_num_1, hex_num_0}),
+	 	.leds_export({hundreds, signs, LEDR}),
+	 	.keycode_export(keycode)
 		
-	//  );
+	);
 
 	logic [1:0] stage_color_index;
 	logic [2:0] sprite_color_index;
@@ -192,8 +192,16 @@ logic Reset_h, vssig, blank, sync, VGA_Clk;
 	// state machine will determine these later on
 	assign spritesheet_x = 4'h1;
 	assign spritesheet_y = 4'h0;
-	assign chef_xcoord = 96;
-	assign chef_ycoord = 141;
+	//assign chef_xcoord = 96;
+	//assign chef_ycoord = 141;
+	
+	chef chef_module (
+		.Reset(Reset_h),
+		.frame_clk(VGA_VS),
+		.keycode(keycode),
+		.ChefX(chef_xcoord),
+		.ChefY(chef_ycoord)
+	);
 
 	always_comb
 	begin
@@ -218,7 +226,14 @@ logic Reset_h, vssig, blank, sync, VGA_Clk;
 		end
 	end
 	
-	stage_ram stages (.data(), .address(ycoord * 640 + xcoord), .wren(1'b0), .clock(MAX10_CLK1_50), .q(stage_color_index));
+	stage_ram stages (
+		.data(), 
+		.address(ycoord * 640 + xcoord), 
+		.wren(1'b0), 
+		.clock(MAX10_CLK1_50), 
+		.q(stage_color_index)
+	);
+	
 	sprite_ram sprites (
 		.data(), 
 		.address((spritesheet_y * 16 + spritesheet_yoffset) * 240 + spritesheet_x * 16 + spritesheet_xoffset), 
